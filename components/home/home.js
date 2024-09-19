4b5da2ba1f6d2465885cedf50d5df62c55f7c117
Mathew-off <matheus.fpr123@gmail.com>
2024-09-18 21:11:04 -0300

    modularização

--- components/home/home.js
+++ components/home/home.js
@@ -1,13 +1,33 @@
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('../../components/header/header.html')
-      .then(response => response.text())
+document.addEventListener("DOMContentLoaded", function() {
+  // Carregar o Header
+  const headerElement = document.getElementById('header-geral');
+  fetch('../header/header.html')
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Erro ao carregar o header.');
+          }
+          return response.text();
+      })
       .then(data => {
-        document.querySelector('header-geral').innerHTML = data;
+          headerElement.innerHTML = data;
+      })
+      .catch(error => {
+          console.error('Erro ao carregar o header:', error);
       });
-  
-    fetch('../../components/footer/footer.html')
-      .then(response => response.text())
+
+  // Carregar o Footer
+  const footerElement = document.getElementById('footer-geral');
+  fetch('../footer/footer.html')
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Erro ao carregar o footer.');
+          }
+          return response.text();
+      })
       .then(data => {
-        document.querySelector('footer-geral').innerHTML = data;
+          footerElement.innerHTML = data;
+      })
+      .catch(error => {
+          console.error('Erro ao carregar o footer:', error);
       });
-  });
+});

0a61123e7cc3e79b649317373fa24c6c49a9808f
wmarcoss <wmarcoos.81@gmail.com>
2024-11-13 19:53:17 -0300

    up

--- components/cadastro/cadastro.js
+++ components/cadastro/cadastro.js
@@ -14,7 +14,7 @@ document.getElementById('cadastroForm').addEventListener('submit', async functio
 
     // Enviar dados para o backend
     try {
-        const response = await fetch('http://localhost:3000/cadastro', {  // Altere para a URL completa do backend
+        const response = await fetch('http://localhost:3000/cadastro', {  
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -25,7 +25,7 @@ document.getElementById('cadastroForm').addEventListener('submit', async functio
         const result = await response.json();
         if (response.ok) {
             alert(result.message);
-            window.location.replace('../login/login.html'); // Redireciona para a mesma aba da página de login
+            window.location.replace('../login/login.html'); 
         } else {
             alert(result.error);
         }
@@ -36,6 +36,7 @@ document.getElementById('cadastroForm').addEventListener('submit', async functio
 
 // Evento para redirecionar ao clicar em "Cancelar"
 document.getElementById('cancelar').addEventListener('click', function() {
+<<<<<<< HEAD
     window.location.replace('../login/login.html'); // Redireciona para a mesma aba da página de login
 });
 
@@ -68,3 +69,7 @@ function toggleConfirmPasswordVisibility() {
         eyeIconConfirm.src = "../../assets/icons/olhin fechado.png"; // ícone para senha oculta
     }
 }
+=======
+    window.location.replace('../login/login.html'); 
+});
+>>>>>>> 74d955f0759eea65d795a4d4a8ce81b976dd96a0
